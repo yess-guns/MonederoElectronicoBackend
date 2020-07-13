bb850dc4c6832f998440119f693d4a5e54549040
Abigail Castañeda Luna <castanedalunaabigail@gmail.com>
2020-07-13 17:57:45 -0500

    creación del componente ListCliente

---
+++ frontendvue/src/components/Clientes/ListCliente.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1>Clientes</h1>
+        <b-button variant="primary">Nuevo</b-button>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-table striped hover :items="clientes" :fields="filas"></b-table>
+    </b-row>
+  </b-container>
+
+  
+  
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      clientes: [],
+      filas: [
+          { key: 'nombreCliente', label: 'Nombre', sortable: true, sortDirection: 'desc' },
+          { key: 'apellidosCliente', label: 'Apellidos' },
+          { key: 'codigo', label: 'Código de barras', sortable: false, sortDirection: 'desc' },
+          { key: 'porcentaje', label: 'Porcentaje descuento ', sortable: false, class: 'text-center' },
+          { key: 'saldoMonedero', label: 'Saldo en monedero', sortable: false, class: 'text-center' },
+          { key: 'estatus', label: 'Estatus', sortable: false, class: 'text-center' }
+        ]
+    }
+  },
+  methods: {
+    getClientes(){
+      const path = 'http://127.0.0.1:8000/api/clientes/'
+      axios.get(path).then((response) => {
+        this.clientes = response.data
+      })
+    }
+  },
+  created() {
+    this.getClientes()
+  }
+}
+</script>
--- frontendvue/src/router/index.js
+++ frontendvue/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import ListCliente from '@/components/Clientes/ListCliente'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +12,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/clientes',
+      name: 'ListCliente',
+      component: ListCliente
     }
-  ]
+  ],
+  mode: 'history'
 })
